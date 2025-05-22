a9e05d6bd7f813d03a04309d81bea9f7f2c9ed2c
Yucsan <yucsan2023@gmail.com>
2025-05-22 23:59:39 +0200

    sube

--- src/main/java/com/aventura/api/mapper/UsuarioMapper.java
+++ src/main/java/com/aventura/api/mapper/UsuarioMapper.java
@@ -3,19 +3,26 @@ package com.aventura.api.mapper;
 import com.aventura.api.dto.UsuarioDTO;
 import com.aventura.api.entity.Usuario;
 import org.mapstruct.*;
+import org.mapstruct.ReportingPolicy;
 
-@Mapper(componentModel = "spring")
+@Mapper(
+  componentModel = "spring",
+  // Esto hace que MapStruct no falle por campos no mapeados
+  unmappedTargetPolicy = ReportingPolicy.IGNORE
+)
 public interface UsuarioMapper {
 
     UsuarioDTO toDTO(Usuario usuario);
 
     Usuario toEntity(UsuarioDTO usuarioDTO);
 
-    // Este método actualiza un Usuario existente con datos del DTO, ignorando los campos protegidos
+    /** 
+     * Actualiza un Usuario existente con datos del DTO, 
+     * ignorando id, fechaRegistro y deletedAt.
+     */
     @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
     @Mapping(target = "id", ignore = true)
     @Mapping(target = "fechaRegistro", ignore = true)
     @Mapping(target = "deletedAt", ignore = true)
     void updateEntityFromDTO(UsuarioDTO dto, @MappingTarget Usuario entity);
 }
-
