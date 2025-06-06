9c03bb179496744e11555a79703e734d76ec798c
Yucsan <yucsan2023@gmail.com>
2025-06-06 19:10:17 +0200

    local casa

--- src/main/java/com/aventura/api/config/WebConfig.java
+++ src/main/java/com/aventura/api/config/WebConfig.java
@@ -14,8 +14,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                		//.allowedOrigins("http://127.0.0.1:5500", "http://localhost:5500")
-                		.allowedOrigins("https://admin-panel-1ojj.onrender.com")
+                		.allowedOrigins("https://admin-panel-1ojj.onrender.com" , "http://127.0.0.1:5500", "http://localhost:5500")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(false);
